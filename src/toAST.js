3ec0618f0242821cd86e77e97f434e7a1a99ad50
dezhizhang <1541609448@qq.com>
2022-06-06 07:13:15 +0800

    feat:tokenrenader

--- src/toAST.js
+++ src/toAST.js
@@ -1,9 +1,24 @@
 const ASTNode = require('./ASTNode');
-let nodeTypes = require('./nodeType');
-
+const nodeTypes = require('./nodeType');
+const tokenType = require('tokenType');
 function toAST(TokenReader) {
     let rootNode = new ASTNode(nodeTypes.Program)
     return rootNode;
+}
+
+function additive(TokenReader) {
+    
+}
+
+function number(TokenReader) {
+    let node = null;
+    let token = TokenReader.peek();
+    if(token !== null && token.type === tokenType.NUMBER) {
+        token = TokenReader.read();
+        node = new ASTNode(nodeTypes.Numeric,token.value);
+    }
+    return node;
+
 }
 
 
